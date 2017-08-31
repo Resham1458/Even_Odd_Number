263ada0680802a3e0202e1e0424fb80d09d59520
Resham1458 <anuresham2015@gmail.com>
2017-08-31 12:14:27 -0400

    updated

--- even_or_odd.py
+++ even_or_odd.py
@@ -8,9 +8,9 @@ def check():
         print("Goodbye!")
 
 def number():
-    number = int(input("Please enter any random number:"))
+    number = int(input("Please enter any random number:")) #asks user to enter random number
 
-    if number % 2 == 0:
+    if number % 2 == 0:                                    #tests for the even number
         print("You entered an even number")
     else:
         print("You entered an odd number")
